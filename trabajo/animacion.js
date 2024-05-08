95af9b1eb63ef28867312b2684047da78ceef27f
GILBERTO S.S <111308513+g1783rt0@users.noreply.github.com>
2024-05-07 23:27:28 -0300

    trabajon de hoja principal

---
+++ trabajo/animacion.js
@@ -0,0 +1,81 @@
+/* 
+let usuLogueado= "jperesz";
+let claveIngresada= "1549"
+
+let usuBD= "admin";
+let claveBD= "3456";
+*/
+/*
+if (usuLogueado == usuBD) {
+    console.log("Usuario Correcto");
+    if(claveIngresada== claveBD){
+        console.log("contraseña Correcta");
+    }
+}else{
+    console.log("Error Usuario Invalido ")
+}*/
+
+/*otra forma de realizar esta operacion*/
+
+/*
+if (usuLogueado == usuBD && claveIngresada== claveBD) {
+    console.log("usuario logueado")
+}else{
+    console.log("usuario incorrecto")
+}
+*/
+
+
+/* siver para verificar si el tipo es de dato es de caracter string o numerico*/
+
+/*
+let a=5;
+let b= 8;
+console.log(a==b);
+console.log(a==b);
+*/
+
+/*
+let nombreAlu= "juan";
+let nota= 10;
+
+if (nota >= 7  && nota <=10){
+    console.log(nombreAlu + "Aprobo la materia");
+}else if( nota >= 4 && nota < 7){
+    console.log(nombreAlu +"Aprobo la materia");
+}else if( nota >=1 && nota < 5){
+    console.log( nombreAlu +"Desaprobo la materia");
+}else{
+    console.log("Nota Incorrecta");
+}
+*/
+/*
+let  x = 0;
+let numero = 7;
+
+console.log("tabla del " + numero);
+while (x <= 10) {
+    console.log( numero + " x " + x + " = "+ x * numero);
+    
+    x = x+i;
+}
+*/
+
+/*clse 15 24 de abril */
+
+let str= prompt("ingrese el texto");
+    function invertirTex(str){
+        let dividir= str.split("");
+        console.log(dividir);
+        let r=rever.reverse();
+        console.log(r);
+        let t = r.join("");
+        console.log(t);
+        return t;
+    }
+
+    
+  
+  
+  
+  
